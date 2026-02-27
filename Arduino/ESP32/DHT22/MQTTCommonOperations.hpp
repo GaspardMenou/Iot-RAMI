45bee8ea1fcd6c18cd3713e1e25856f6727f7ee3
Gaspard Menou <gaspard.menou@gmail.com>
2026-02-27 10:23:45 +0100

    révision de la structure pour les script arduino avec un script de base à réutiliser pour les esp32

--- Arduino/ESP32/DHT22/MQTTCommonOperations.hpp
+++ Arduino/ESP32/DHT22/MQTTCommonOperations.hpp
@@ -26,6 +26,8 @@
 extern const char* COMMAND_PING;
 extern const char* COMMAND_START;
 extern const char* COMMAND_STOP;
+extern const char* COMMAND_ACK;
+
 // possible answers
 extern const char* PING_RESPONSE;
 extern const char* PING_RESPONSE_WHEN_ALREADY_PUBLISHING;
@@ -36,6 +38,8 @@ extern const char* MSG_TIMESTAMP;
 extern const char* MSG_CMD;
 extern const char* MSG_ANS;
 extern const char* MSG_VALUE;
+extern const char* MSG_MEASURE;
+
 
 /****** NTP Client Settings; PROGREM because these settings are configured only once at the beginning *******/
 extern const char* NTP_SERVER PROGMEM;
@@ -50,11 +54,16 @@ void setCACertForTLS(WiFiClientSecure& client, const char* certificate);
 void reconnect(PubSubClient& client, const char* mqtt_username, const char* mqtt_password, const char* topic);
 void publishJSONMessage(PubSubClient& client, const char* topic, const char* json_buffer, const bool& retained=true);
 long long getCurrentMicrosecondTimestampLong();
+void sendPing(PubSubClient& client, const char* topic,const bool& retained=false);
+void sendStart(PubSubClient& client, const char* topic,const bool& retained=false);
+void sendStop(PubSubClient& client, const char* topic,const bool& retained=false);
 void publishAnswerToServerCommand(PubSubClient& client, const char* topic, const String& answer, const bool& retained=true);
 void publishValue(PubSubClient& client, const char* topic, const float& value, const bool& retained=true);
+void publishMeasures(PubSubClient& client, const char* topic, const char* measureTypes[], const float measures[],int count, const bool& retained=true);
 void handlePingCommand(PubSubClient& client, const char* topic, const bool& allow_to_publish);
 void handleStartCommand(PubSubClient& client, const char* topic, bool& allow_to_publish);
 void handleStopCommand(PubSubClient& client, const char* topic, bool& allow_to_publish);
 void interactWithReceivedCommand(PubSubClient& client, const String& received_command, const char* topic, bool& allow_to_publish);
+void interactWithAnswerCommand(PubSubClient& client, const String& received_command, const char* topic, bool& allow_to_publish);
 
 #endif // MQTT_COMMON_OPERATIONS
