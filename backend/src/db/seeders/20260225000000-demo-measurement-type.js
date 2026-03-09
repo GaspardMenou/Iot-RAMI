381f0785d0f9be2fc1caee2c482ba78c722e4f78
Gaspard Menou <gaspard.menou@gmail.com>
2026-03-09 15:12:25 +0100

    feat: ajouter un type de mesure pour la pression dans le seeder

--- backend/src/db/seeders/20260225000000-demo-measurement-type.js
+++ backend/src/db/seeders/20260225000000-demo-measurement-type.js
@@ -7,6 +7,7 @@ module.exports = {
       { id: "ab11102a-3aa9-4f50-9055-0c81e69102e5", name: "ecg" },
       { id: "15de12e8-d2da-4a2f-9f37-17fad8622f22", name: "temperature" },
       { id: "a06f3a9b-41de-4828-a19c-1802ba1dfbd4", name: "humidity" },
+      { id: "d1c9b8e7-5a3c-4f0e-9b1a-2f8e5c6a7f33", name: "pressure" },
     ]);
   },
   async down(queryInterface, Sequelize) {
